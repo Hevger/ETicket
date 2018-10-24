369dbf7d250231a91d24937c212f99d7619a9de9
Klaus Skovsmose Nielsen <1067822@ucn.dk>
2018-10-24 12:39:50 +0200

    Added Seat class

---
+++ ETicket/TestClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbEvent db = new DbEvent();
+            db.CreateEvent();
+        }
+    }
+}
