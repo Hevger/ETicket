4e4a923a7d8995dcb83640c85d6a985221d77bf2
Hefkr Salim Ibrahim <1067318@ucn.dk>
2018-10-30 11:06:25 +0100

    Created Interface

--- ETicket/DataAccess/DbEvent.cs
+++ ETicket/DataAccess/DbEvent.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -8,24 +10,76 @@ using System.Threading.Tasks;
 
 namespace DataAccess
 {
-    public class DbEvent
+    public class DbEvent : ICRUD
     {
         string connectionString = ConfigurationManager.ConnectionStrings["ETicketDb"].ConnectionString;
 
-
-        public void CreateEvent()
+        // Create Event 
+        public void Create(Object obj)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
                 using (SqlCommand command = connection.CreateCommand())
                 {
-                    command.CommandText = "Insert into Event values ('Haiwan','Dance or dont', 'F0',  '2018-10-24',  '2018-10-24',  '2018-10-24', 30, 35.00)";
+                    Event myEvent = (Event)obj;
+                    command.CommandText = "Insert into Event (Title, Description, Gate, GateOpens, StartTime, Date, AvailableTickets, TicketPrice) values (@Title, @Description, @Gate, @GateOpens, @StartTime, @Date, @AvailableTickets, @TicketPrice)";
+                    command.Parameters.AddWithValue("Title", myEvent.Title);
+                    command.Parameters.AddWithValue("Description", myEvent.Description);
+                    command.Parameters.AddWithValue("Gate", myEvent.Gate);
+                    command.Parameters.AddWithValue("GateOpens", myEvent.GateOpens);
+                    command.Parameters.AddWithValue("StartTime", myEvent.StartTime);
+                    command.Parameters.AddWithValue("Date", myEvent.Date);
+                    command.Parameters.AddWithValue("AvailableTickets", myEvent.AvailableTickets);
+                    command.Parameters.AddWithValue("TicketPrice", myEvent.TicketPrice);
                     command.ExecuteNonQuery();
                 }
             }
         }
-        
+
+
+
+        // Get Event
+        public object Get(int id = 0)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                Event newEvent = null;
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "Select * from Event where EventId = @id";
+                    command.Parameters.AddWithValue("EventId", id);
+
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        newEvent = new Event
+                        {
+                            EventId = reader.GetInt32(reader.GetOrdinal("EventId")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            Gate = reader.GetString(reader.GetOrdinal("Gate")),
+                            GateOpens = reader.GetDateTime(reader.GetOrdinal("GateOpens")),
+                            StartTime = reader.GetDateTime(reader.GetOrdinal("StartTime")),
+                            Date = reader.GetDateTime(reader.GetOrdinal("Date")),
+                            AvailableTickets = reader.GetInt32(reader.GetOrdinal("AvailableTickets")),
+                            TicketPrice = reader.GetDecimal(reader.GetOrdinal("TicketPrice"))
+                        };
+                        }
+                    return newEvent;
+                }
+            }
+        }
+
+
+        // Delete Event 
+
+
+
+        // Update Event
+
 
     }
 
--- ETicket/TestClient/Program.cs
+++ ETicket/TestClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess;
+using Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,24 @@ namespace TestClient
         static void Main(string[] args)
         {
             DbEvent db = new DbEvent();
-            db.CreateEvent();
+            Event myEvent = new Event()
+            {
+                Title = "Test",
+                Description = "Kharai 3elikm",
+                Gate = "8b8",
+                GateOpens = DateTime.Now,
+                StartTime = DateTime.Now,
+                Date = DateTime.Now,
+                AvailableTickets = 30,
+                TicketPrice = 35.50M
+            };
+       
+            db.Create(myEvent);
+
+            Console.WriteLine("-----");
+
+            Event newEvent = (Event) db.Get(7);
+            Console.WriteLine(newEvent);
         }
     }
 }
