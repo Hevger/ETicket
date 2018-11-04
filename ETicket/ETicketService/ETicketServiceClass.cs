55cad2b3ec43a4cccfd8e33f4ea2fae09d401214
darwishmammo <34026849+darwishmammo@users.noreply.github.com>
2018-11-04 04:10:58 +0100

    Added a dedicated host project and successfully hosted ETicketService

---
+++ ETicket/ETicketService/ETicketServiceClass.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLogic;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicketService
+{
+    public class ETicketServiceClass : ICustomerService, IEventService
+    {
+        private CustomerController customerC = new CustomerController();
+        private EventController eventC = new EventController();
+
+        // Create Customer
+        public void CreateCustomer(Customer customer)
+        {
+
+        }
+
+        // Delete Customer
+        public void DeleteCustomer(int id)
+        {
+        }
+
+        // Get Customer
+        public Customer GetCustomer(int id)
+        {
+            return (Customer)customerC.Get(id);
+        }
+
+        // Update Customer
+        public void UpdateCustomer(Customer customer)
+        {
+        }
+
+        public void CreateEvent(Event myEvent)
+        {
+            eventC.Create(myEvent);
+        }
+
+        // Delete Event
+        public void DeleteEvent(int id)
+        {
+            eventC.Delete(id);
+        }
+
+
+        // Get Event 
+        public Event GetEvent(int id)
+        {
+            return (Event)eventC.Get(id);
+        }
+
+        // Update Event
+        public void UpdateEvent(Event myEvent)
+        {
+            eventC.Update(myEvent);
+        }
+    }
+}
